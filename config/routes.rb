cbce3b17d77b9e2d2df9b41042f016d66ff601c7
Lee Jones <scribblethink@gmail.com>
2010-08-23 23:09:42 -0400

    Set root route to properties

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :property_types
-
-  map.resources :photos
-
   map.resources :properties
+  map.resources :photos
+  map.resources :property_types
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -37,13 +35,13 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "properties", :action => "index"
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  # map.connect ':controller/:action/:id'
+  # map.connect ':controller/:action/:id.:format'
 end
