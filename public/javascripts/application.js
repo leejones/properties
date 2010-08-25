2dc86622b5c9c2ff40653a5a45a530aaba90f297
Lee Jones <scribblethink@gmail.com>
2010-08-25 00:57:58 -0400

    Add tabs to properties list page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+	var tabContainers = $('div.tabs > div');
+	tabContainers.hide().filter(':first').show();
+	
+	$('ul.tabnavigation a').click(function(){
+		tabContainers.hide().filter(this.hash).show();
+		$('ul.tabnavigation a').removeClass('selected');
+		$(this).addClass('selected');
+		return false;
+	}).filter(':first').click(0);
+});
