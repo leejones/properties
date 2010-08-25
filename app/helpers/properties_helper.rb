f83c484fa5eb03a311736fff56f1120dd0ca7d72
Lee Jones <scribblethink@gmail.com>
2010-08-25 00:18:10 -0400

    Move google maps url to helper

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,6 @@
 module PropertiesHelper
+  def link_to_google_maps(text, property)
+    address = [property.street, property.city, property.state, property.zip].map { |a| CGI::escape(a.to_s).gsub(/\s/, '+') }.join(', ')
+    link_to text, "http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=#{address}&z=17"
+  end
 end
