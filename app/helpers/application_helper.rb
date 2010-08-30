3a0564a8df6b10560eccfc6e494353b5b90aa9ad
Lee Jones <scribblethink@gmail.com>
2010-08-30 00:44:43 -0400

    Fix markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def markdown(text)
+    text.blank? ? "" : BlueCloth.new(text).to_html.html_safe
+  end
 end
