3c5a373d4fde6e394e99d3dc1dc6d3024a73ceab
Lee Jones <scribblethink@gmail.com>
2010-08-29 23:18:52 -0400

    Upgrade to Rails 3
    * Replace constant with Rails.logger
    * Back up files before upgrade
    * Run rails3 generator
    * Update user model for rails 3
    * Update routes
    * Remove new defaults initializer
    * Remove public/index.html
    * Add required gems to gemfile
    * Use scope instead of named_scope
    * Fix markdown
    * Fix forms for rails 3
    * Removed rails2 backup files
    * Update Rails version and use mysql2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,17 @@
+require 'yaml'
+
 class User
-
   def self.all
-    YAML.load_file(File.join(File.dirname(__FILE__), '../../config', 'users.yml'))[:users]
-  end
-
-  def self.authenticate(username, password)
-    if all[username] == password
-      logger.info "User '#{username}' logged in."
-      return true
-    else
-      logger.warn "User '#{username}' failed to log in."
-      return false
-    end
+    YAML.load_file(File.join(File.dirname(__FILE__), '../..', 'config', 'users.yml'))[:users]
   end
   
-  def self.logger
-    RAILS_DEFAULT_LOGGER
+  def self.authenticate(username, password)
+    if all[username] == password
+      Rails.logger.info "User '#{username}' logged in."
+      return true
+    else
+      Rails.logger.warn "User '#{username}' failed to log in."
+      return false
+    end
   end
 end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Properties::Application.config.secret_token = '1a4abf1b28a45fade814ae210e8e50ba401f7361ca1a3d64ca32ac0d695a207d0756f392182e5a3a23f0e01fa64110286f52266aa611df3d36c0c1143663a489'
