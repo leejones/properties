4478c328c83f0f5b1af47e1884900da171daad2e
Lee Jones <scribblethink@gmail.com>
2010-08-29 23:18:52 -0400

    Replace constant with Rails.logger

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,11 @@
 class User
   def self.authenticate(username, password)
     if USERS[username] == password
-      logger.info "User '#{username}' logged in."
+      Rails.logger.info "User '#{username}' logged in."
       return true
     else
-      logger.warn "User '#{username}' failed to log in."
+      Rails.logger.warn "User '#{username}' failed to log in."
       return false
     end
-  end
-  
-  def self.logger
-    RAILS_DEFAULT_LOGGER
   end
 end
